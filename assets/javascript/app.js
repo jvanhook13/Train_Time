1d0e2f87f02ec52d1888f7a946e07f0e38a63272
jvanhook13 <jaredvanhook1313@gmail.com>
2020-03-09 14:31:50 -0700

    html done using boot strap for header and form and table

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+
+//firebase config
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA5zN5cB6gEPgI_GKTEwyW879RFolKxMJQ",
+    authDomain: "yeethaw-44fcf.firebaseapp.com",
+    databaseURL: "https://yeethaw-44fcf.firebaseio.com",
+    projectId: "yeethaw-44fcf",
+    storageBucket: "yeethaw-44fcf.appspot.com",
+    messagingSenderId: "956768172316",
+    appId: "1:956768172316:web:3fb62cd266f48e524922d9",
+    measurementId: "G-NQW60SZ9TL"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database() ;
+//snapshot the values of a form
+//push snapshot to database
+//use moment to get itme
+//set interval to train schedule
+//calculate till next train
+//display with jquery
+
+
+
+
+
+})
