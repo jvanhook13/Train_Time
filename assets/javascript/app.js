5b2b9367b7a6b9c6401f34f926349f82e68853c8
jvanhook13 <jaredvanhook1313@gmail.com>
2020-03-11 23:35:00 -0700

    Bugged my appending so it add weird now. Added moment.js logic. is bugged and doesnt work. adding psuedocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-//firebase config
-// Your web app's Firebase configuration
-var firebaseConfig = {
+  //firebase config
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyA5zN5cB6gEPgI_GKTEwyW879RFolKxMJQ",
     authDomain: "yeethaw-44fcf.firebaseapp.com",
     databaseURL: "https://yeethaw-44fcf.firebaseio.com",
@@ -13,70 +13,90 @@ var firebaseConfig = {
     appId: "1:956768172316:web:3fb62cd266f48e524922d9",
     measurementId: "G-NQW60SZ9TL"
   };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database() ;
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
-//intial values
-var name 
-var destination 
-var frequency 
-var firstTrain 
-var minAway 
+  //intial values
+  var name
+  var destination
+  var frequency
+  var firstTrain
+  var minAway
 
-//Button Click
-$("#search").on("click", function(event) {
-  event.preventDefault()
+  //Button Click
+  $("#search").on("click", function (event) {
+    event.preventDefault()
 
 
-//grabs from form
- name = $("#Train-name").val().trim()
- console.log(name) 
- destination = $("#Destination").val().trim()
- console.log(destination)
- frequency = $("#frequency").val().trim()
- console.log(frequency)
- firstTrain = $("#firstTrain").val().trim()
- console.log(firstTrain)
- 
- database.ref().push({
-  name: name ,
-  destination: destination ,
-  frequency: frequency ,
-  firstTrain: firstTrain
-  
-  
+    //grabs from form
+    name = $("#Train-name").val().trim()
+    console.log(name)
+    destination = $("#Destination").val().trim()
+    console.log(destination)
+    frequency = $("#frequency").val().trim()
+    console.log(frequency)
+    firstTrain = $("#firstTrain").val().trim()
+    console.log(firstTrain)
+
+    database.ref().push({
+      name: name,
+      destination: destination,
+      frequency: frequency,
+      firstTrain: firstTrain
+
+
+    })
+
+    //snapshot the values of a form
+    database.ref().on("child_added", function (snapshot) {
+
+      var sv = snapshot.val();
+
+      console.log("name", sv.name)
+      console.log("destination", sv.destination)
+      console.log("frequency", sv.frequency)
+      console.log("firstTrain", sv.firstTrain)
+
+      $("#nameTrain").append(sv.name);
+      $("#destinations").append(sv.destination);
+      $("#frequencies").append(sv.frequency);
+      $("#firstTrain").append(sv.firstTrain);
+
+
+      var convertTime = moment(firstTrain, "HH:mm").subtract(1, "years")
+        console.log("convertedtime" , convertTime)
+
+      var currentTime = moment() ;
+        console.log(currentTime , "current")
+
+    
+      var timeDiff = moment().diff(moment(convertTime), "minutes")
+      console.log("time diff" , timeDiff)
+
+      var timeApart = timeDiff % frequency ;
+
+      var timeAway = frequency - timeApart ; 
+      console.log(timeAway)
+
+      var nextArrival = moment().add(timeAway, "minutes") ; 
+      console.log("next arrival" , nextArrival)
+
+      
+
+
+
+    })
+
+
   })
-  
-  //snapshot the values of a form
-  database.ref().on("child_added" , function(snapshot){
-  
-    var sv = snapshot.val() ;
-  
-    console.log("name" , sv.name)
-    console.log("destination" , sv.destination)
-    console.log("frequency" , sv.frequency)
-    console.log("firstTrain" , sv.firstTrain)
-  
-    $("#nameTrain").text(sv.name) ;
-    $("#destinations").text(sv.destination) ;
-    $("#frequencies").text(sv.frequency) ;
-    $("#firstTrain").text(sv.firstTrain) ;
-  
-  
-  
-  
-  })
 
+  //push to database
 
-})
-
-//push to database
-
-//use moment to get itme
-//set interval to train schedule
-//calculate till next train
-//display with jquery
+  //use moment to get itme
+  //set interval to train schedule
+  //calculate till next train
+  //display with jquery
 
 
 
